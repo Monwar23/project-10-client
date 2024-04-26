3120629fa88f565e2769b4b58b99e5f223f18c0c
monwar23 <monwar2345@gmail.com>
2024-04-27 01:20:44 +0600

    firebase provider created and added in main jsx file

--- src/firebase/FirebaseProvider.jsx
+++ src/firebase/FirebaseProvider.jsx
@@ -1,9 +1,21 @@
+import { getAuth } from "firebase/auth";
+import { createContext } from "react";
+import app from "./firebase.config";
 
+export const AuthContext=createContext(null)
+const auth=getAuth(app)
 
 const FirebaseProvider = ({children}) => {
+
+    const authInfo={
+
+    }
+
     return (
         <div>
-            
+            <AuthContext.Provider value={authInfo}>
+            {children}
+            </AuthContext.Provider>
         </div>
     );
 };
