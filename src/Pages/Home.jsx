4594a996547803a56a08568b74eb3c4385978b16
monwar23 <monwar2345@gmail.com>
2024-04-29 15:00:01 +0600

    Artgallery section completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,54 +5,65 @@ import ArtCraftSection from "../components/ArtCraftSection";
 import { useEffect, useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import { Helmet } from "react-helmet";
+import ArtGallery from "../components/ArtGallery";
 
 
 const Home = () => {
 
-    const [arts,setArts]=useState([])
+    const [arts, setArts] = useState([])
 
-    const crafts=useLoaderData()
+    const crafts = useLoaderData()
 
     useEffect(() => {
 
         fetch('http://localhost:5000/artSection')
-          .then(res => res.json())
-          .then(data => {
-            setArts(data)
-          })
-      }, []);
+            .then(res => res.json())
+            .then(data => {
+                setArts(data)
+            })
+    }, []);
 
     return (
         <div>
-             <Helmet>
+            <Helmet>
                 <title>ArtHive || Home</title>
             </Helmet>
-        <Banner></Banner>
+            <Banner></Banner>
 
-        <Fade>
-        <div className="mt-10">
-            <h2 className="text-3xl text-blue-400 font-bold text-center">Welcome to our Craft Items section! </h2>
-            <p className="text-center mt-4"> Explore a variety of beautifully crafted items that will spark your creativity and add a touch of charm to your projects. </p>
-        </div>
-        </Fade>
-       
-       <div className="grid md:grid-cols-2 gap-5 mt-10">
-       {
-            crafts.slice(0,6).map(craft=> <CraftItemSection key={craft._id} craft={craft}></CraftItemSection> )
-        }
-       </div>
-       <Fade>
-        <div className="mt-14">
-            <h2 className="text-3xl text-blue-400 font-bold text-center">
-Explore Art & Craft Subcategories</h2>
-            <p className="text-center mt-4">Dive into the world of creativity with our diverse range of art & craft subcategories. Each subcategory offers unique opportunities for expression and discovery.</p>
-        </div>
-        </Fade>
-       <div className="grid md:grid-cols-2 gap-5 mt-10">
-        {
-            arts.slice(0,6).map(art=><ArtCraftSection key={art._id} art={art}></ArtCraftSection>)
-        }
-       </div>
+            <Fade>
+                <div className="mt-10">
+                    <h2 className="text-3xl text-blue-400 font-bold text-center">Our Craft Items ! </h2>
+                    <p className="text-center mt-4"> Explore a variety of beautifully crafted items that will spark your creativity and add a touch of charm to your projects. </p>
+                </div>
+            </Fade>
+
+            <div className="grid md:grid-cols-2 gap-5 mt-10">
+                {
+                    crafts.slice(0, 6).map(craft => <CraftItemSection key={craft._id} craft={craft}></CraftItemSection>)
+                }
+            </div>
+            <Fade>
+                <div className="mt-14">
+                    <h2 className="text-3xl text-blue-400 font-bold text-center">
+                        Explore Art & Craft Subcategories</h2>
+                    <p className="text-center mt-4">Dive into the world of creativity with our diverse range of art & craft subcategories. <br /> Each subcategory offers unique  opportunities for expression and discovery.</p>
+                </div>
+            </Fade>
+            <div className="grid md:grid-cols-2 gap-5 mt-10">
+                {
+                    arts.slice(0, 6).map(art => <ArtCraftSection key={art._id} art={art}></ArtCraftSection>)
+                }
+            </div>
+
+            <Fade>
+                <div className="mt-14">
+                    <h2 className="text-3xl text-blue-400 font-bold text-center">
+                    OUR BRANDS !</h2>
+                    <p className="text-center mt-4">From basketmakers to weavers and everything in between, we have got you <br /> covered. Browse the Directory to find artists from across the country and discover something new.</p>
+                </div>
+            </Fade>
+            <ArtGallery></ArtGallery>
+
         </div>
     );
 };
---
+++ src/components/ArtGallery.jsx
@@ -0,0 +1,72 @@
+import Marquee from "react-fast-marquee";
+
+const ArtGallery = () => {
+    return (
+        <div className="mt-10">
+            <Marquee pauseOnHover={true} speed={100}>
+                <div className="mr-16">
+                    <img className="h-14 w-32 rounded-lg" src="https://i.ibb.co/9ZXrTzJ/proxxon.jpg" alt="Proxxon" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/RNdzSJ6/vangogh.jpg" alt="Van Gogh" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/GFvdQM2/Holzkraft.jpg" alt="Holzkraft" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/cxLMs7z/Staedtler-Logo-svg.jpg" alt="Staedtler" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/0JRh799/bullseye-glass.jpg" alt="Bullseye Glass" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/M1K6Xvn/beavercraft.jpg" alt="Beavercraft" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/YXfv67S/Bruynzeel1.jpg" alt="Bruynzeel" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/5nhTbQ0/herbin-logo.jpg" alt="Herbin" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/m69g8DL/inland-logo.jpg" alt="Inland" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/Pgh4YJp/artclay-logo.jpg" alt="Art Clay" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/frSsNS3/fenice-logo.jpg" alt="Fenice" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/6s4DnKG/amati.jpg" alt="Amati" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/sKkYNNz/kemper-logo.jpg" alt="Kemper" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/yn1p6ys/schott-logo1.jpg" alt="Schott" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/r3JQgLX/helen-Breil-logo.jpg" alt="Helen Breil" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/hyFcntg/hegner-logo.jpg" alt="Hegner" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/MDT9b5f/Y-LOGO.jpg" alt="Y Logo" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/JxkZDfj/double-helix-logo.jpg" alt="Double Helix" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/jD03dYg/herbert-arnold-logo.jpg" alt="Herbert Arnold" />
+                </div>
+                <div className="mr-16">
+                    <img className="h-14 w-28 rounded-lg" src="https://i.ibb.co/281hN05/silhouette.jpg" alt="Silhouette" />
+                </div>
+            </Marquee>
+        </div>
+    );
+};
+
+export default ArtGallery;
