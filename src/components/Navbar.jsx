7aabba5b4ffcb8258759eeb4e012e1be22627a63
monwar23 <monwar2345@gmail.com>
2024-04-29 16:26:17 +0600

    login register and logout button updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import UseAuth from "../Hooks/UseAuth";
 import { useEffect, useState } from "react";
+import DDD from "./DDD";
 
 
 const Navbar = () => {
@@ -72,30 +73,28 @@ const Navbar = () => {
                             title={user.displayName}
                             className="rounded-full h-10 w-10 cursor-pointer"
                         />
-                        <button
-                            className="btn bg-blue-600 text-white text-lg ml-2"
-                            onClick={handleSignOut}
-                        >
-                            Log Out
+                        <button onClick={handleSignOut} className="relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 ml-2 rounded-md group">
+                            <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-blue-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+                            <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+                            <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-blue-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+                            <span className="relative text-blue-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Log Out</span>
                         </button>
                     </div>
                 ) : (
                     <>
-                        <Link to="/login">
-                            <button className="btn bg-blue-600 text-white text-lg">
-                                Login
-                            </button>
+                        <Link to="/login" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group mr-3">
+                            <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                            <span className="relative w-full text-left text-blue-600 font-bold transition-colors duration-300 ease-in-out group-hover:text-white">Login</span>
                         </Link>
-                        <Link to="/register">
-                            <button className="btn bg-blue-600 text-white text-lg ml-2">
-                                Register
-                            </button>
+                        <Link to="/register" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                            <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                            <span className="relative w-full text-left text-blue-600 font-bold transition-colors duration-300 ease-in-out group-hover:text-white">Register</span>
                         </Link>
                     </>
                 )}
                 <label className="cursor-pointer grid place-items-center ml-3">
                     <input onChange={toggleTheme} type="checkbox" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-                    checked={theme === 'dark'} />
+                        checked={theme === 'dark'} />
                     <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                     <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                 </label>
